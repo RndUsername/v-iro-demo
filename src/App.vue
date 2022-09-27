d6debb8e45fa100de714f573be6665090d816a6e
duckstape <39034509+RndUsername@users.noreply.github.com>
2022-09-27 04:04:11 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div ref="wrapper"></div>
+  <input type="checkbox" name="disablePicker" v-model="disable">
+  <label for="disablePicker">Disable</label> <br>
+  Color: {{color}}
+</template>
+
+<script setup lang="ts">
+import { ref, type Ref } from 'vue';
+import { useIroColorPicker } from "v-iro.js"
+import type { IroColor } from '@irojs/iro-core';
+
+const wrapper = ref()
+const color = ref("")
+const disable = ref(false)
+
+const options = {
+    width: 130
+}
+
+useIroColorPicker(wrapper, options, disable).then((colorPicker) => {
+  colorPicker.on("color:change", (pickerColor: IroColor) => {
+    color.value = pickerColor.hexString
+  })
+})
+</script>
+
+<style scoped>
+</style>
